b19114d9239ad9679edf9babc8b97e32da0dc788
home <nuttapong_coach@hotmail.com>
2024-09-27 21:13:50 +0700

    first commit

---
+++ API/Services/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+using API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Client> Clients {get; set;}
+    }
+}
